a9d0e9ebad647535f14287d90f20b8763b06f335
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-13 13:07:01 +0700

    fixed error

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://chill-movie-react-meir.vercel.app",
+    ],
+  })
+);
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
