6d6c41891f58331e0e84e8da12fb3d8692a6ebc6
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-13 13:58:37 +0700

    error

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://chill-movie-react-meir.vercel.app",
-    ],
-  })
-);
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
