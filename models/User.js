9cf1334e7bd8377d4d4e63537ea2076127cad737
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-13 15:37:18 +0700

    fixed error v2

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    email: { type: String, default: "" },
+    email: { type: String, unique: true, sparse: true },
     password: { type: String, required: true },
     imgUser: { type: String, default: "" },
   },
