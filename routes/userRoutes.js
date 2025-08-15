74f24c9dec42c9f5e15b300f3dfdb9c620069a46
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-15 13:36:10 +0700

    fixed error2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {
-  registerUser,
-  loginUser,
-  getUsers,
-} = require("../controllers/userController");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 // Register user
 router.post("/register", registerUser);
 
 // Login user
 router.post("/login", loginUser);
-
-router.get("/", getUsers);
 
 module.exports = router;
